3df14fa1b2f026d839a9361021ce1f8afb8bc1b8
Vaibhavibhalke <142711357+Vaibhavibhalke@users.noreply.github.com>
2024-03-07 10:46:58 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Hello\n";
+  cout << "Hello VAIBHAVI \n";
   
   cout << "I have successfully built and run\n";
   return 0;
