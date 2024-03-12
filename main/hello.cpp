2e06e1442f751906ca4d8cb97cf59e1409d17dbd
VAIBHAVI <142711357+Vaibhavibhalke@users.noreply.github.com>
2024-03-12 22:36:05 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Hello VAIBHAVI_BHALKE \n";
+  cout << "Hello VAIBHAVI_Mahindrakar \n";
   
   cout << "I have successfully built and run\n";
   return 0;
